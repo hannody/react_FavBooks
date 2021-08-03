20c2d9e26baf4cfd1ec19fc78c0db3950ac591e0
hannody <aljaboori@gmail.com>
2021-08-03 18:52:57 +0800

    React basic tutorial

--- src/index.js
+++ src/index.js
@@ -1,17 +1,50 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDom from 'react-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const books = [
+    {
+        id: 1,
+        img:
+            'https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg',
+        title: 'I Love You to the Moon and Back',
+        author: 'Amelia Hepworth',
+    },
+    {
+        id: 2,
+        img:
+            'https://images-na.ssl-images-amazon.com/images/I/71aLultW5EL._AC_UL200_SR200,200_.jpg',
+        title: 'Our Class is a Family',
+        author: 'Shannon Olsen',
+    },
+    {
+        id: 3,
+        img:
+            'https://images-na.ssl-images-amazon.com/images/I/71e5m7xQd0L._AC_UL200_SR200,200_.jpg',
+        title: 'The Vanishing Half: A Novel',
+        author: 'Brit Bennett',
+    },
+];
+const Book = ({img, title, author}) => {
+    return (
+        <article className='book'>
+            <img src={img} alt=''/>
+            <h2>{title}</h2>
+            <h4>{author}</h4>
+        </article>
+    );
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function BookList() {
+    return (
+        <section className='booklist'>
+            {
+                books.map((book) => {
+                    return <Book key={book.id} {...book}></Book>
+                })
+            }
+        </section>
+    );
+}
+
+ReactDom.render(<BookList />, document.getElementById('root'));
